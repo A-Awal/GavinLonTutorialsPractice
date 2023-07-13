11e97cf8ae097eb7c9f46c21292fbb32a3c906a0
A-Awal <abdulai.awal@amalitech.org>
2023-07-13 09:10:08 +0000

    First commit

---
+++ BasicDeligateExample/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    delegate void LogDel(string text);
+    public static void Main(string[] args)
+    {
+        // Practicing Simgle Delegates
+        // //LogDel logDel = new LogDel(LogToTextFile);
+        // Console.WriteLine("Please enter your name");
+        //
+        // var userName = Console.ReadLine();
+        //
+        // logDel(userName);
+        //
+        // Console.ReadKey();
+        
+        //MultiCast Delegates
+
+        Log log = new Log();
+        LogDel logToscreen, logTofile;
+        logToscreen = new LogDel(log.LogtextToScreen);
+        logTofile = new LogDel(log.LogToTextFile);
+        LogDel multilog = logTofile + logToscreen;
+        
+        Console.WriteLine("Please enter your name");
+        
+        var userName = Console.ReadLine();
+        
+        multilog(userName);
+        
+        Console.ReadKey();
+    }
+
+    static void LogtextToScreen(string text)
+    {
+        Console.WriteLine($"{DateTime.Now}:{text}");
+    }
+
+    static void LogToTextFile(string text)
+    {
+        using (StreamWriter writer = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "text.txt"), true))
+        {
+            writer.WriteLine($"{DateTime.Now}:{text}");
+        }
+    }
+
+}
+
+class Log
+{
+    public void LogtextToScreen(string text)
+    {
+        Console.WriteLine($"{DateTime.Now}:{text}");
+    }
+
+    public void LogToTextFile(string text)
+    {
+        using (StreamWriter writer = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "text.txt"), true))
+        {
+            writer.WriteLine($"{DateTime.Now}:{text}");
+        }
+    }
+
+}
